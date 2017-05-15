a1e23cc1fc90c4c40e9bfdc71a4572270312f2f9
Cyanoluck <cyanoluck@gmail.com>
2017-05-15 22:51:04 +0300

    templ

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/**
+ * Created by vladshushpanov on 15.05.17.
+ */
+
+
+
+$("#testimonail-carousel").owlCarousel({
+    autoPlay: false, //Set AutoPlay to 3 seconds
+    items : 3,
+    stopOnHover : true,
+    navigation : false, // Show next and prev buttons
+    pagination : true
+});
